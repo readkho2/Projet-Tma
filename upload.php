04af7c3af237c5f51da3386ad17df2e1fc23d79d
readkho2 <redakhouya041@gmail.com>
2024-03-06 11:10:33 +0100

    Your commit message here

---
+++ upload.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["profile_pic"])) {
+    $uploadDir = "Dossier-dest/"; // Dossier spécifique où les fichiers seront sauvegardés
+
+    $uploadedFile = $_FILES["profile_pic"];
+    $fileName = basename($uploadedFile["name"]);
+    $targetPath = $uploadDir . $fileName;
+
+    if (move_uploaded_file($uploadedFile["tmp_name"], $targetPath)) {
+        echo "Le fichier a été téléchargé avec succès dans le dossier spécifique.";
+    } else {
+        echo "Une erreur s'est produite lors du téléchargement du fichier.";
+    }
+} else {
+    echo "Erreur : Aucun fichier n'a été envoyé.";
+}
+?>
