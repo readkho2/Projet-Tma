a841041780a4d84804b297b24352db3aa36882c5
tvinchent-epsi <thibault.vinchent@campus-cd.com>
2024-03-08 17:15:58 +0100

    cours MA TMA 1 a 3

---
+++ v2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Epsilon - v2</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="square">
+        <?php include('header.php') ?>
+        
+        <img src="Tibo.jpg" alt="Un Thibault connu..">
+        <p>Prénom NOM</p>
+        <p>Groupe : Yohann DEVRED, Guillaume LENOBLE</p>
+        
+        <a href="upload.php">Upload your file !</a>
+
+        <p>Copyright © Espi Lille 2024</p>
+    </div>
+</body>
+</html>
---
+++ v3/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Epsilon - v3</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="square">
+        <?php include('header.php') ?>
+        
+        <table>
+            <tr>
+                <td>
+                    <img src="Tibo.jpg" alt="Un Thibault connu..">
+                    <p>Prénom NOM</p>
+                </td>
+                <td>
+                    <img src="thibault.jpg" alt="Un autre Thibault connu..">
+                    <p>Prénom NOM</p>
+                </td>
+            </tr>
+        </table>
+
+        <p>10/20 : facilement modifiable mais j'ai trouvé le CSS mal organisé.</p>
+        
+        <a href="upload.php">Upload your file !</a>
+
+        <?php include('footer.php') ?>
+    </div>
+</body>
+</html>
